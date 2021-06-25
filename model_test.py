cafc3fd05dc65c03474f2d3a55f7f00bb4eedcc6
Ulto85 <74577196+Ulto85@users.noreply.github.com>
2021-06-25 08:34:39 -0700

    Update model_test.py

--- model_test.py
+++ model_test.py
@@ -1,4 +1,4 @@
-
+from processingutils import processing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
@@ -7,7 +7,14 @@ import random
 import pandas as pd
 import pickle
 import numpy as np
+tfid = pickle.load(open('audaxlabs_project_1/vectorizer.pickle','rb'))
+texts = [processing('I want 5 burgers')]
+vectors= tfid.transform(texts)
 
-kneighbors = pickle.load(open('audaxlabs_project_1/model.pickle'))
-_,_,x_test,y_test = pickle.load(open('audaxlabs_project_1/data.pickle'))
-print(accuracy_score(y_test,kneighbors.predict(x_test)))
+kneighbors = pickle.load(open('audaxlabs_project_1/model.pickle','rb'))
+_,_,x_test,y_test = pickle.load(open('audaxlabs_project_1/data.pickle','rb'))
+print(x_test)
+pred  = kneighbors.predict(x_test)
+print(pred)
+print(y_test)
+print(kneighbors.predict(vectors))
